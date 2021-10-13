507e10e9ea4e134578eb5a300d696aa7251aec67
Alex <oleksandr.bondarenko.qa@gmail.com>
2021-10-13 10:45:59 +0300

    added chapter 2 homework solution

---
+++ JS_For_Kids_Homework/chapter2.js
@@ -0,0 +1,47 @@
+// 1) правильный подсчет нужного кол-ва шариков
+
+let balloonsCorrectCount = (15 + 9) * 2;
+console.log(baloonsCorrectCount);
+
+//2) операторы
+
+let balloons = 100;
+balloons *= 2;
+console.log(balloons);
+
+let balloons = 100;
+balloons /= 4;
+console.log(balloons);
+
+//3) проверка возраста и сопровождения
+
+let isAccessGrantedCheck;
+
+if (age >= 13 || accompanied == true) {
+    isAccessGrantedCheck = console.log("Access granted")
+}
+else {
+    isAccessGrantedCheck = console.log("Access denied")
+}
+return isAccessGrantedCheck;
+
+let kids = [kidOne,kidTwo,kidThree];
+
+kidOne = {
+    age = 12,
+    accompanied = true
+};
+
+kidTwo = {
+    age = 13,
+    accompanied = false
+}
+
+kidThree = {
+    age = 12,
+    accompanied = false
+}
+
+kids.isAccessGrantedCheck;
+
+
